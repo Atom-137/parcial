529240bee535b82ca6bc58e88e0108c4db4dd1d7
Ath0m <correoadfr@gmail.com>
2022-03-13 15:44:17 -0600

    Agregando ordenar burbuja

--- parcial1.1/parcial1.1.cpp
+++ parcial1.1/parcial1.1.cpp
@@ -25,79 +25,73 @@ struct cola
 };
 */
 
-//void ordenar(persona *);
+void ordenar(persona[], int n);
+void mostrarArregloOrdenado(persona *, int n);
 
 void ingresarDatos() {
 
     cout << "Ingrese el numero de personas: " << endl;
     cin >> n;
     const int nSize = 50;
-    persona personas[50],  *ptr_persona;
-    ptr_persona = personas;
+    persona personas[50];
+
 
     for (int i = 0; i < n; i++) {
 
         cout << "Ingrese el nombre: " << endl;
-        cin >> (personas+i)->nombre;
+        cin >> personas[i].nombre;
         cout << "Ingrese el apellido: " << endl;
-        cin >> (personas + i)->apellido;
+        cin >> personas[i].apellido;
         cout << "Ingrese la edad: " << endl;
-        cin >> (personas + i)->edad;
+        cin >> personas[i].edad;
         cout << "Ingrese la nacionalidad: " << endl;
-        cin >> (personas + i)->nacionalidad;
+        cin >> personas[i].nacionalidad;
     }
 
     //Mostrar los datos
 
-    for (int i = 0; i < n; i++) {
-
-        cout << "Nombre: " << endl;
-        cout << (personas + i)->nombre;
-        cout << "Apellido: " << endl;
-        cout << (personas + i)->apellido;
-        cout << "Edad: " << endl;
-        cout << (personas + i)->edad;
-        cout << "Nacionalidad: " << endl;
-        cout << (personas + i)->nacionalidad;
-    }
-
-   ordenar(personas, n);
+     ordenar(personas, n);
 
 }
 
 
-void ordenar(persona *ptr_persona, int nSize) {
+void ordenar(persona arregloPersonas[], int nSize) {
 
-    
+
     persona aux;
+    persona *ptr_persona = arregloPersonas;
 
-   for (int pasada = 0; pasada < nSize - 1; pasada++)
+    for (int pasada = 0; pasada < nSize - 1; pasada++)
+    {
+        for (int k = 0; k < nSize - 1; k++)
         {
-            for (int k = 0; k < nSize - 1; k++)
+            if ((ptr_persona + k)->edad >(ptr_persona + (k + 1))->edad)
             {
-                if ((ptr_persona+k) ->edad > (ptr_persona +(k+1))->edad)
-                {
-                    aux = *(ptr_persona + k);
-                    *(ptr_persona + k) = *(ptr_persona + k + 1);
-                    *(ptr_persona + k+ 1) = aux;
-                }
+                aux = *(ptr_persona + k);
+                *(ptr_persona + k) = *(ptr_persona + (k + 1));
+                *(ptr_persona + (k + 1)) = aux;
             }
         }
+    }
     mostrarArregloOrdenado(ptr_persona, nSize);
 
 }
 
-void mostrarArregloOrdenado( persona *ptr_personas, int nElementos) {//Mostrando Arreglo ordenado
+void mostrarArregloOrdenado(persona *ptr_personas, int nElementos) {//Mostrando Arreglo ordenado
     cout << "\n\nMostrando Arreglo Ordenado: ";
     for (int i = 0; i < nElementos; i++) {
-        cout << ptr_personas + i << " ";
+        cout << endl << (ptr_personas + i)->nombre << " ";
+        cout << (ptr_personas + i)->apellido << " ";
+        cout << (ptr_personas + i)->edad << " ";
+        cout << (ptr_personas + i)->nacionalidad << " " << endl;
+
     }
 }
 
 
 int main()
 {
-    
+
     ingresarDatos();
 
 
